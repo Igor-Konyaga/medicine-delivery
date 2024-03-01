5feccef26a1f0060304f777aee61a29d0fd7a97b
Ihor_Koniaha <konyaga.igor10@gmail.com>
2024-03-01 18:52:27 +0200

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 
-import { AppWrapper } from './App.styled';
-
 const test = import.meta.env.VITE_API_TEST;
 
 function App() {
   console.log(test);
   return (
-    <AppWrapper>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}></Route>
-      </Routes>
-    </AppWrapper>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}></Route>
+    </Routes>
   );
 }
 export default App;
