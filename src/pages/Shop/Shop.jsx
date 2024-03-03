d8b8986675f1b2a17c7afce7713ab9389572f78e
Ihor_Koniaha <konyaga.igor10@gmail.com>
2024-03-04 00:25:44 +0200

    update

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,12 +1,9 @@
-import { MedicineList } from '../../components/MedicineList/MedicineList';
-import { ShopList } from '../../components/shopList/shopList';
 import { StyledShopPage } from './Shop.styled';
 
 export const Shop = () => {
   return (
     <StyledShopPage>
-      <ShopList />
-      <MedicineList />
+     
     </StyledShopPage>
   );
 };
